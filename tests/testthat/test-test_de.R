14f279f0bc6868eb32a406f0e927446c47ea8810
const-ae <artjom31415@googlemail.com>
2022-08-18 16:18:40 +0200

    Fix 'test-test_de'

--- tests/testthat/test-test_de.R
+++ tests/testthat/test-test_de.R
@@ -4,17 +4,13 @@ test_that("test_differential_expression works", {
   fit <- differential_embedding(dat, design = ~ condition,
                                 n_ambient = 5, n_embedding = 3, verbose = FALSE)
 
-  fit2 <- estimate_variance(fit, n_bootstrap_samples = 20)
+  fit <- estimate_variance(fit, n_bootstrap_samples = 3)
+  fit <- fit[,1:10]
 
-  res <- test_differential_expression(fit2, fact(condition = "a") == fact(condition = "b"))
-  res2 <- test_differential_expression(fit2, fact(condition = "a") - fact(condition = "b"))
-  res3 <- test_differential_expression(fit2, -conditionb)
-  head(res)
+  res <- test_differential_expression(fit, fact(condition = "b") == fact(condition = "a"))
+  res2 <- test_differential_expression(fit, conditionb)
 
-  parse_contrast(fact(condition = "a") - fact(condition = "b"), colnames(fit$design_matrix), fit$design)
-  parse_contrast(conditionb, colnames(fit$design_matrix), fit$design)
-
-  plot(res3$pval, res2$pval, log = "xy")
-
+  expect_equal(dim(res), dim(res2))
+  expect_equal(res[, c("feature", "obs")], res2[, c("feature", "obs")])
 })
 
