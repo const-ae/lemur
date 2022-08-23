2b9144d4fb3018d894c57c7f53530170be18ed3d
const-ae <artjom31415@googlemail.com>
2022-08-23 11:35:59 +0200

    Include an 'align_embeddings' steps in unit test for DE

--- tests/testthat/test-test_de.R
+++ tests/testthat/test-test_de.R
@@ -3,6 +3,7 @@ test_that("test_differential_expression works", {
   dat <- make_synthetic_data(n_genes = 30, n_cells = 500, n_lat = 3, n_centers = 5)
   fit <- differential_embedding(dat, design = ~ condition,
                                 n_ambient = 5, n_embedding = 3, verbose = FALSE)
+  fit <- align_embeddings(fit, alignment = dat$cell_type)
 
   fit <- estimate_variance(fit, n_bootstrap_samples = 3)
   fit <- fit[,1:10]
