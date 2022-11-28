16beccbdbf001ea6918152ceedacfb9483be0cdc
const-ae <artjom31415@googlemail.com>
2022-11-28 10:11:37 +0100

    Add 'center' argument for 'pca()'

--- R/pca.R
+++ R/pca.R
@@ -1,20 +1,24 @@
 
 
-pca <- function(Y, n){
-
+pca <- function(Y, n, center = TRUE){
+  center_ind <- center
   min_dim <- min(dim(Y))
 
   # Center
-  center <- MatrixGenerics::rowMeans2(Y)
+  center <- if(center_ind){
+    MatrixGenerics::rowMeans2(Y)
+  }else{
+    rep(0, nrow(Y))
+  }
 
   if(n == 0){
     res <- list(rotation = matrix(nrow = nrow(Y), ncol = 0), x = matrix(nrow = 0, ncol = ncol(Y)))
   }else if(min_dim <= n * 2){
     # Do exact PCA
-    res <- prcomp(t(Y), rank. = n, center = TRUE, scale. = FALSE)
+    res <- prcomp(t(Y), rank. = n, center = center_ind, scale. = FALSE)
   }else{
     # Do approximate PCA
-    res <- irlba::prcomp_irlba(t(Y), n = n, center = TRUE, scale. = FALSE)
+    res <- irlba::prcomp_irlba(t(Y), n = n, center = center_ind, scale. = FALSE)
   }
   list(coordsystem = unname(res$rotation), embedding = unname(t(res$x)), offset = center)
 }
