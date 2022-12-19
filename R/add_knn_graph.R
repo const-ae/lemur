8abff478242380db8d26cd96680b18bb160f5c2c
const-ae <artjom31415@googlemail.com>
2022-12-19 16:51:33 +0100

    Fix bug in 'add_knn_graph'

--- R/add_knn_graph.R
+++ R/add_knn_graph.R
@@ -2,7 +2,7 @@
 
 add_knn_graph <- function(fit, k, knn_matrix = NULL){
   if(is.null(knn_matrix)){
-    knn_matrix <-   BiocNeighbors::findKNN(t(fit$diffemb_embedding), k = 4, BNPARAM = BiocNeighbors::AnnoyParam())$index
+    knn_matrix <-   BiocNeighbors::findKNN(t(fit$diffemb_embedding), k = k, BNPARAM = BiocNeighbors::AnnoyParam())$index
   }
   stopifnot(ncol(knn_matrix) == k)
   stopifnot(nrow(knn_matrix) == ncol(fit))
