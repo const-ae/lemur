d29f4e0df9a6f025f1c867080675e8bb3a4a247a
const-ae <artjom31415@googlemail.com>
2023-01-10 14:09:19 +0100

    Factor out make_knn_graph from add_knn_graph

--- R/add_knn_graph.R
+++ R/add_knn_graph.R
@@ -1,13 +1,25 @@
 
-
-add_knn_graph <- function(fit, k, knn_matrix = NULL){
+make_knn_graph <- function(fit, k, knn_matrix = NULL){
   if(is.null(knn_matrix)){
     knn_matrix <-   BiocNeighbors::findKNN(t(fit$diffemb_embedding), k = k, BNPARAM = BiocNeighbors::AnnoyParam())$index
   }
   stopifnot(ncol(knn_matrix) == k)
   stopifnot(nrow(knn_matrix) == ncol(fit))
 
-  graph <- bluster::neighborsToKNNGraph(knn_matrix)
+  # Adapted from
+  # bluster::neighborsToKNNGraph(knn_matrix, directed = TRUE)
+  start <- as.vector(row(knn_matrix))
+  end <- as.vector(knn_matrix)
+  interleaved <- as.vector(rbind(start, end))
+  igraph::make_graph(interleaved, directed = TRUE)
+}
+
+add_knn_graph <- function(fit, k, knn_matrix = NULL, knn_graph = NULL){
+  if(is.null(knn_graph)){
+    graph <- make_knn_graph(fit, k = k, knn_matrix = knn_matrix)
+  }else{
+    graph <- knn_graph
+  }
   metadata(fit)$knn_graph <- graph
   fit
 }
