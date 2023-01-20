2e946bc1efcce66edbc212613f7dcece3e988b24
const-ae <artjom31415@googlemail.com>
2023-01-20 11:17:41 +0100

    Implement weighted ridge regression

--- R/ridge_regression.R
+++ R/ridge_regression.R
@@ -1,11 +1,26 @@
 
-ridge_regression <- function(Y, X, ridge_penalty){
+#' Ridge regression
+#'
+#' The function does not treat the intercept special.
+#'
+#' @param Y the observations matrix (`features x samples`)
+#' @param X the design matrix (`samples x covariates`)
+#' @param ridge_penalty a numeric vector or matrix of size (`covariates` or
+#'   `covariates x covariates` respectively)
+#' @param weights a vector of observation weights
+#'
+#'
+#' @keywords internal
+ridge_regression <- function(Y, X, ridge_penalty = 0, weights = rep(1, nrow(X))){
+  stopifnot(length(weights) == nrow(X))
   if(! is.matrix(ridge_penalty)){
     ridge_penalty <- diag(ridge_penalty, nrow = ncol(X))
   }
-  ridge_penalty_sq <- t(ridge_penalty) %*% ridge_penalty
-  X_extended <- rbind(X, ridge_penalty_sq)
-  Y_extended <- cbind(Y, matrix(0, nrow = nrow(Y), ncol = ncol(X)))
+  ridge_penalty_sq <- sqrt(sum(weights)) * (t(ridge_penalty) %*% ridge_penalty)
+  weights_sqrt <- sqrt(weights)
+
+  X_extended <- rbind(X * weights_sqrt, ridge_penalty_sq)
+  Y_extended <- cbind(t(t(Y) * weights_sqrt), matrix(0, nrow = nrow(Y), ncol = ncol(X)))
   qr <- qr(X_extended)
   res <- t(solve(qr, t(Y_extended)))
   colnames(res) <- colnames(X)
--- tests/testthat/test-ridge_penalty.R
+++ tests/testthat/test-ridge_penalty.R
@@ -6,3 +6,14 @@ test_that("ridge_regression works", {
   expect_equal(ridge_regression(Y, X, ridge_penalty = 0), lm_fit, ignore_attr = "dimnames")
   expect_lt(sum(ridge_regression(Y, X, ridge_penalty = 3)^2), sum(lm_fit^2))
 })
+
+test_that("weighted ridge_regression works", {
+  Y <- randn(5, 30)
+  X <- randn(30, 2)
+  weights <- rexp(n = 30, rate = 2)
+  weights <- weights / sum(weights) * ncol(X)
+
+  lm_fit <- t(coef(lm(t(Y) ~ X - 1, weights = weights)))
+  expect_equal(ridge_regression(Y, X, ridge_penalty = 0, weights = weights), lm_fit, ignore_attr = "dimnames")
+  expect_lt(sum(ridge_regression(Y, X, ridge_penalty = 3, weights = weights)^2), sum(lm_fit^2))
+})
