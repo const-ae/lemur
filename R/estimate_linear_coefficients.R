e0dec2cd3150cf11e151c3cc7a63b722e7222bdc
const-ae <artjom31415@googlemail.com>
2023-04-19 14:53:34 +0200

    Fix edge case in estimate_linear_coefficients for "cluster_median"

--- R/estimate_linear_coefficients.R
+++ R/estimate_linear_coefficients.R
@@ -9,12 +9,14 @@ estimate_linear_coefficient <- function(Y, design_matrix, method = c("linear", "
   }else if(method == "zero"){
     matrix(0, nrow = nrow(Y), ncol = ncol(design_matrix))
   }else if(method == "cluster_median"){
+    min_cluster_membership <- 0.01
     pca <- lemur:::pca(Y, n = 20)
     harm_obj <- lemur:::harmony_init(pca$embedding, design_matrix, nclust = 30, verbose = FALSE)
     harm_obj <- lemur:::harmony_max_div_clustering(harm_obj)
     Yt <- as.matrix(t(Y))
     coef <- do.call(cbind, lapply(seq_len(nrow(harm_obj$R)), \(cl){
-      sel <- harm_obj$R[cl, ] > 0.1
+      threshold <- min(min_cluster_membership, max(harm_obj$R) * 0.5)
+      sel <- harm_obj$R[cl, ] > threshold
       tryCatch({
         fit <- lm.wfit(design_matrix[sel,,drop=FALSE], y = Yt[sel,,drop=FALSE], w = harm_obj$R[cl, sel])
         as.numeric(t(fit$coefficients))
