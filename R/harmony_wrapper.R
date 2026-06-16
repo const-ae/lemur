9676a1c567039ac0e6ad36606d147b1d61d335bd
Hugo Gruson <git@hugogruson.fr>
2026-06-16 16:10:16 +0200

    Use new harmony interface in harmony_wrapper.R
    Co-authored-by: Tim Herbermann <tim.herbermann@embl.de>

--- R/harmony_wrapper.R
+++ R/harmony_wrapper.R
@@ -1,5 +1,4 @@
-
-
+# Modified harmony_init that also returns phi and setup args
 harmony_init <- function(embedding, design_matrix,
                          theta = 2, lambda = 1, sigma = 0.1, nclust = min(round(ncol(embedding) / 30), 100),
                          tau = 0, block.size = 0.05,  max.iter.cluster = 200,
@@ -25,20 +24,36 @@ harmony_init <- function(embedding, design_matrix,
 
 
   sigma <- rep_len(sigma, nclust)
-  lambda_range = c(0.1, 10)
-  if(packageVersion("harmony") < "1.2.0"){
-    stop("Your 'harmony' version is outdated: ", packageVersion("harmony"), ". Please update to version >= 1.2.0")
-  }else{
-    alpha <- 0.2
-    harmonyObj <- harmony::RunHarmony(embedding, mm_groups, nclust = nclust, max.iter = 0, return_object = TRUE, verbose = FALSE)
-    harmonyObj$setup(
-      embedding, phi,
-      sigma, theta, lambda_vec, alpha, max.iter.cluster, epsilon.cluster,
-      epsilon.harmony, nclust, block.size, B_vec, verbose
-    )
-  }
+  alpha <- 0.2
+  batch.prop.cutoff <- 1e-5
+
+  harmonyObj <- harmony::RunHarmony(embedding, mm_groups, nclust = nclust, 
+                                     max_iter = 0, return_object = TRUE, 
+                                     verbose = FALSE)
+  harmonyObj$setup(
+    embedding, phi, sigma, theta, lambda_vec, alpha, max.iter.cluster, 
+    epsilon.cluster, epsilon.harmony, nclust, block.size, B_vec, 
+    batch.prop.cutoff, verbose
+  )
+  
   harmony_init_clustering(harmonyObj)
-  harmonyObj
+  
+  # Return object AND the setup args needed to reinitialize embedding later
+  list(
+    obj = harmonyObj,
+    phi = phi,
+    sigma = sigma,
+    theta = theta,
+    lambda_vec = lambda_vec,
+    alpha = alpha,
+    max.iter.cluster = max.iter.cluster,
+    epsilon.cluster = epsilon.cluster,
+    epsilon.harmony = epsilon.harmony,
+    nclust = nclust,
+    block.size = block.size,
+    B_vec = B_vec,
+    batch.prop.cutoff = batch.prop.cutoff
+  )
 }
 
 #' Create an arbitrary Harmony object so that I can modify it later
@@ -54,7 +69,9 @@ harmony_new_object <- function(){
 
 harmony_init_clustering <- function(harmonyObj, iter.max = 25, nstart = 10){
   stopifnot(is(harmonyObj, "Rcpp_harmony"))
-  harmonyObj$Y <- t(stats::kmeans(t(harmonyObj$Z_cos), centers = harmonyObj$K, iter.max = iter.max, nstart = nstart)$centers)
+  Z_corr <- harmonyObj$getZcorr()
+  Z_cos <- sweep(Z_corr, 2, sqrt(colSums(Z_corr^2)), "/")
+  harmonyObj$Y <- t(stats::kmeans(t(Z_cos), centers = harmonyObj$K, iter.max = iter.max, nstart = nstart)$centers)
   harmonyObj$init_cluster_cpp()
   harmonyObj
 }
